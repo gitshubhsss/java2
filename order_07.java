4eea749fd21b9a0440c972db0aa07aad98789383
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-23 22:39:54 +0530

    .\order_07.java

---
+++ order_07.java
@@ -0,0 +1,35 @@
+package herarchical_programs;
+import java.util.*;
+class order_01
+{
+	double orderid;
+	String decreption;
+	order_01(double orderid,String decreption)
+	{
+		this.orderid=orderid;
+		this.decreption=decreption;
+	}
+	void display()
+	{
+		System.out.println("order id : "+orderid);
+		System.out.println("description : "+decreption);
+	}
+}
+class purchase_od
+{
+	String cxname;
+	purchase_od(double orderid,String decreption)
+	{
+		this.cxname=cxname;
+		super(orderid,decreption);
+		
+	}
+}
+public class order_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
